9efbe4348aea55f1560399c816db87c667e72566
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-01-20 20:00:26 +0530

    style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import Trivia from './components/Trivia'
 
 const App = () => {
+  
   return (
-    <div className=''>
+    <div className='relative'>
+      
     <div className=' relative'>
+    <div className='absolute text-gray-100 p-4'>
+        <div className='pt-[300px]'><h1 className='w-28 h-28 border-8  rounded-full p-6 text-5xl'>30</h1></div>
+        <h1 className='pt-[300px]'><Trivia /></h1>
+      </div>
         {/* Overlay */}
         <div className='pl-[1200px]'>
         <div className=' absolute w-[350px] h-full text-gray-200  bg-[#23235b] flex flex-col  pl-2'>
+          
             <h1 className='pt-20    text-xl'>15<span className='font-bold px-20'>$ 1000000</span></h1>
             <h1 className='   text-xl pt-2'>14<span className='font-bold px-20'>$ 500000</span></h1>
             <h1 className='   text-xl pt-2'>13<span className='font-bold px-20'>$ 250000</span></h1>
---
+++ src/components/Trivia.js
@@ -0,0 +1,13 @@
+export default function Trivia()  {
+  return (
+    <div className="Trivia">
+      <div className="questions">What's the best youtube channel?</div> 
+      <div className="answers"></div> 
+      <div className="answer">Lama Dev</div>
+      <div className="answer">Lama Dev</div>
+      <div className="answer">Lama Dev</div>
+      <div className="answer">Lama Dev</div>
+    </div>
+  )
+}
+
