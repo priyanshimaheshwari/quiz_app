763700a6282fdb0ad2f5f39bc23d9b3ce5fdc983
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-01-31 17:26:19 +0530

    trivia

--- src/App.js
+++ src/App.js
@@ -1,35 +1,107 @@
 import React, { useState } from 'react'
+import "./App.css";
+import Trivia from './components/Trivia';
 
 const App = () => {
   const [questionNumber,setQuestionNumber]=useState(1)
+  const[stop,setStop]=useState(false)
+  const data = [
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answers: [
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetic",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "When did the website `Facebook` launch?",
+      answers: [
+        {
+          text: "2004",
+          correct: true,
+        },
+        {
+          text: "2005",
+          correct: false,
+        },
+        {
+          text: "2006",
+          correct: false,
+        },
+        {
+          text: "2007",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+  ];
+
   const moneyPyramid=[
-    {id:1,amount:"$100"},
-    {id:2,amount:"$200"},
-    {id:3,amount:"$300"},
-    {id:4,amount:"$400"},
-    {id:5,amount:"$500"},
-    {id:6,amount:"$1000"},
-    {id:7,amount:"$2000"},
-    {id:8,amount:"$4000"},
-    {id:9,amount:"$8000"},
-    {id:10,amount:"$16000"},
-    {id:11,amount:"$32000"},
-    {id:12,amount:"$64000"},
-    {id:13,amount:"$125000"},
-    {id:14,amount:"$250000"},
-    {id:15,amount:"$500000"},
+    {id:1,amount:"  $ 100"},
+    {id:2,amount:"  $ 200"},
+    {id:3,amount:"  $ 300"},
+    {id:4,amount:"  $ 400"},
+    {id:5,amount:"  $ 500"},
+    {id:6,amount:"  $ 1000"},
+    {id:7,amount:"  $ 2000"},
+    {id:8,amount:"  $ 4000"},
+    {id:9,amount:"  $ 8000"},
+    {id:10,amount:"$ 16000"},
+    {id:11,amount:"$ 32000"},
+    {id:12,amount:"$ 64000"},
+    {id:13,amount:"$ 125000"},
+    {id:14,amount:"$ 250000"},
+    {id:15,amount:"$ 500000"},
   ].reverse()
   return (
     <div className='h-[800px] flex bg-[#020230] text-white'>
-      <div className='w-3/4 h-full'><img className='w-full h-full items-center' src='https://www.medianews4u.com/wp-content/uploads/2020/02/KBC-1.jpg' alt='/'/>
-      <div>
-        <div></div></div></div>
-      <div className='w-1/4 flex items-center justify-center'>
+      <div className='w-3/4 h-full'><img className='w-3/4 h-[800px] items-center flex absolute' src='https://www.medianews4u.com/wp-content/uploads/2020/02/KBC-1.jpg' alt='/'/>
+      <div className='top'>
+        <div className='timer'>30</div><div className=' pt-[350px]'><Trivia data={data} setStop={setStop} questionNumber={questionNumber}  setQuestionNumber={setQuestionNumber}/></div></div></div>
+      <div className='w-1/4 h-[800px] flex items-center justify-center relative'>
         <ul className='w-full p-4'>
           {moneyPyramid.map((m)=>(
-          <li className={questionNumber===m.id?"bg-teal-400 rounded-xl":"bg-[#020230]"}><div className='flex items-center justify-between  rounded p-2'>
+          <li className={questionNumber===m.id?"bg-teal-400 rounded-xl":"bg-[#020230]"}><div className='flex  item-center  rounded p-2'>
           <span className='text-xl  '>{m.id}</span>
-          <span className='text-2xl '>{m.amount}</span></div></li>
+          <span className='text-2xl pl-16 '>{m.amount}</span></div></li>
           ))}
             </ul>
             </div>
--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,12 +1,49 @@
-export default function Trivia()  {
+import { useEffect, useState } from "react"
+
+
+export default function Trivia({
+  data,
+  questionNumber,
+  setQuestionNumber,
+  setStop,
+}) {
+  const [question, setQuestion] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [className, setClassName] = useState("answer");
+
+  useEffect(() => {
+    setQuestion(data[questionNumber - 1]);
+  }, [data, questionNumber]);
+
+  const delay=(duration,callback)=>{
+       setTimeout(()=>{
+         callback();
+       }, duration);
+  };
+  const handleClick = (a) => {
+    setSelectedAnswer(a);
+    setClassName("answer active");
+    delay(3000,()=>
+      setClassName(a.correct?"answer correct":"answer wrong")
+    );
+    delay(6000,()=>{
+      
+    },3000);
+    setTimeout(()=>{
+      setClassName(a.correct?"answer correct":"answer wrong")
+    },3000);
+}
   return (
-    <div className="Trivia">
-      <div className="questions">What's the best youtube channel?</div> 
-      <div className="answers"></div> 
-      <div className="answer">Lama Dev</div>
-      <div className="answer">Lama Dev</div>
-      <div className="answer">Lama Dev</div>
-      <div className="answer">Lama Dev</div>
+    <div className='pl-[500px] pr-[200px] font-extrabold '>
+      <div className="bg-gradient-to-b from-violet-900 to-black text-center p-4 border rounded-xl">{question?.question}</div> 
+      <div className="pt-[80px]  flex flex-wrap justify-between ">{question?.answers.map((a)=>(
+       <div
+       className={selectedAnswer === a ? className : "answer"}
+       onClick={() => !selectedAnswer && handleClick(a)}
+     >
+          {a.text}</div>
+      ))}
+      </div> 
     </div>
   )
 }
