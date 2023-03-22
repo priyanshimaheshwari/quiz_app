125ba3ce20f16d118c119f3c781c3f13b07f75b5
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-03-22 22:18:18 +0530

    bg

--- src/components/Start.js
+++ src/components/Start.js
@@ -11,7 +11,7 @@ export default function Start({ setUsername }) {
     <div className="flex flex-col w-[200px] h-[100px] relative m-auto items-center justify-around ">
         <input className="w-[200px] h-[30px] rounded-md text-center  outline-none text-black text-lg bg-slate-50 " placeholder="Enter your name" 
         ref={inputRef}/>
-        <button className="w-[200px] h-[30px] rounded-md cursor-pointer text-lg bg-slate-50 text-black" onClick={handleClick}>Start</button>
+        <button className="w-[200px] h-[30px] rounded-md cursor-pointer text-lg bg-slate-100 text-black" onClick={handleClick}>Start</button>
     </div>
   )
 }
